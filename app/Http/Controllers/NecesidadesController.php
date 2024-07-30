9b5ad9a12ff149ef0372e1494ff63bc3a916bb1c
abimaelfv <abimaelfv@hotmail.com>
2024-07-30 01:47:11 -0500

    Roles por usuario

--- app/Http/Controllers/NecesidadesController.php
+++ app/Http/Controllers/NecesidadesController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Necesidades;
+use App\Models\User;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 
 class NecesidadesController extends Controller
 {
     public function index()
     {
+        $this->authorize('necesidades.ver');
+
         $necesidades = Necesidades::all();
         return view('necesidades', compact('necesidades'));
     }
--- app/Http/Controllers/PropuestasController.php
+++ app/Http/Controllers/PropuestasController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Propuestas;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PropuestasController extends Controller
 {
     public function index()
     {
-        return view('propuestas');
+        $propuestas = $this->getPropuestas();
+        return view('propuestas', compact('propuestas'));
+    }
+
+    public function getPropuestas()
+    {
+        if (User::esRol('ESTUDIANTE')) {
+            $propuestas = Propuestas::where('pro_estado', 1)->get();
+        } else {
+            $propuestas = Propuestas::where('pro_estado', 1)->get();
+        }
+        return $propuestas ?? [];
     }
 }
