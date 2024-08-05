6a7734325e756ac4b0b1b69060259f7e176872b9
MaDDaIt <2021110395@udh.edu.pe>
2024-08-05 18:55:47 -0500

    Cambiar arriba a la derecha la fecha registrada

--- resources/views/livewire/necesidades/card-necesidades.blade.php
+++ resources/views/livewire/necesidades/card-necesidades.blade.php
@@ -3,12 +3,16 @@
         @foreach ($necesidades as $necesidad)
             <div class="bg-white shadow-sm rounded-lg p-6 flex justify-between items-center card-item">
                 <div class="w-full">
+                    <div class="flex justify-between items-center">
+                        <div></div>
+                        <p class="text-gray-400 text-sm font-bold">Registrado el {{ $necesidad->nec_created->format('d/m/Y') ?? '' }}</p>
+                    </div>
+                    
                     <h3 class="text-lg font-medium mb-2 text-blue-950">{{ $necesidad->nec_titulo }}</h3>
                     <p class="text-gray-500 mb-4 line-clamp-3">{{ $necesidad->nec_descripcion }}</p>
                     <div class="flex justify-between items-end mt-2">
                         <div class="text-gray-400 text-sm font-bold block md:flex">
                             <x-com_proceso :status="$necesidad->nec_proceso" />
-                            <p class="pl-3 pt-1">Registrado el {{ $necesidad->nec_created->format('d/m/Y') ?? '' }}</p>
                         </div>
                         <div>
                             <button wire:click="verNecesidad({{ $necesidad->nec_id }})"
