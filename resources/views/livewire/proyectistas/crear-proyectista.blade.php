64852960bf7aaece1f98788a9721b5fbfb4ddaee
abimaelfv <abimaelfv@hotmail.com>
2024-08-10 01:48:02 -0500

    Estilos

--- resources/views/livewire/proyectistas/crear-proyectista.blade.php
+++ resources/views/livewire/proyectistas/crear-proyectista.blade.php
@@ -13,38 +13,38 @@
 
         <x-slot name="content">
             <div class="mt-6">
-                <x-label for="name" value="{{ __('Nombres') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" wire:model="name" />
+                <x-label for="name" value="{{ __('Full Names') }}" />
+                <x-input id="name" class="block mt-1 w-full" type="text" autocomplete="off" wire:model="name" />
                 <x-input-error for="name" class="mt-2" />
             </div>
 
             <div class="mt-6">
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" wire:model="email" />
+                <x-input id="email" class="block mt-1 w-full" type="email" autocomplete="off" wire:model="email" />
                 <x-input-error for="email" class="mt-2" />
             </div>
 
             <div class="mt-6">
                 <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" wire:model="password" />
+                <x-input id="password" class="block mt-1 w-full" type="password" autocomplete="off" wire:model="password" />
                 <x-input-error for="password" class="mt-2" />
             </div>
 
             <div class="mt-6">
                 <x-label for="telefono" value="{{ __('Teléfono') }}" />
-                <x-input id="telefono" class="block mt-1 w-full" type="text" wire:model="telefono" />
+                <x-input id="telefono" class="block mt-1 w-full" type="text" autocomplete="off" wire:model="telefono" />
                 <x-input-error for="telefono" class="mt-2" />
             </div>
 
             <div class="mt-6">
                 <x-label for="profesion" value="{{ __('Profesión') }}" />
-                <x-input id="profesion" class="block mt-1 w-full" type="text" wire:model="profesion" />
+                <x-input id="profesion" class="block mt-1 w-full" type="text" autocomplete="off" wire:model="profesion" />
                 <x-input-error for="profesion" class="mt-2" />
             </div>
 
             <div class="mt-6">
                 <x-label for="descripcion" value="{{ __('Más detalles') }}" />
-                <x-textarea id="descripcion" class="block mt-1 w-full" type="text" wire:model="descripcion"
+                <x-textarea id="descripcion" class="block mt-1 w-full" type="text" autocomplete="off" wire:model="descripcion"
                     placeholder="Agrega más detalles del proyectista." />
                 <x-input-error for="descripcion" class="mt-2" />
             </div>
--- resources/views/livewire/proyectistas/editar-proyectista.blade.php
+++ resources/views/livewire/proyectistas/editar-proyectista.blade.php
@@ -6,32 +6,32 @@
 
         <x-slot name="content">
             <div class="mt-6">
-                <x-label for="name" value="{{ __('Nombres') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" wire:model.defer="name" />
+                <x-label for="name" value="{{ __('Full Names') }}" />
+                <x-input id="name" class="block mt-1 w-full" type="text" autocomplete="off" wire:model.defer="name" />
                 <x-input-error for="name" class="mt-2" />
             </div>
 
             <div class="mt-6">
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" wire:model.defer="email" />
+                <x-input id="email" class="block mt-1 w-full" type="email" autocomplete="off" wire:model.defer="email" />
                 <x-input-error for="email" class="mt-2" />
             </div>
 
             <div class="mt-6">
                 <x-label for="telefono" value="{{ __('Teléfono') }}" />
-                <x-input id="telefono" class="block mt-1 w-full" type="text" wire:model.defer="telefono" />
+                <x-input id="telefono" class="block mt-1 w-full" type="text" autocomplete="off" wire:model.defer="telefono" />
                 <x-input-error for="telefono" class="mt-2" />
             </div>
 
             <div class="mt-6">
                 <x-label for="profesion" value="{{ __('Profesión') }}" />
-                <x-input id="profesion" class="block mt-1 w-full" type="text" wire:model.defer="profesion" />
+                <x-input id="profesion" class="block mt-1 w-full" type="text" autocomplete="off" wire:model.defer="profesion" />
                 <x-input-error for="profesion" class="mt-2" />
             </div>
 
             <div class="mt-6">
                 <x-label for="descripcion" value="{{ __('Más detalles') }}" />
-                <x-textarea id="descripcion" class="block mt-1 w-full" type="text" wire:model.defer="descripcion"
+                <x-textarea id="descripcion" class="block mt-1 w-full" type="text" autocomplete="off" wire:model.defer="descripcion"
                 placeholder="Agrega más detalles del proyectista." />
                 <x-input-error for="descripcion" class="mt-2" />
             </div>
