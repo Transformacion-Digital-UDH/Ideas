dccaf31119bd3ec03db4174bb2cfb3e6f4f8e283
Erickmela <2021110411@udh.edu.pe>
2024-08-09 19:11:32 -0500

    Todos los filtros listos

--- resources/views/necesidades.blade.php
+++ resources/views/necesidades.blade.php
@@ -5,13 +5,19 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 space-y-4">
         <div class="max-w-screen-2xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <div class="flex justify-between items-center pb-3">
+                    <div></div> <!-- Espacio vacío para mantener la alineación -->
+                    <x-input-buscar data-table="table_id" type="text" placeholder="Buscar..." class="light-table-filter" />
+                </div>
                 @livewire('necesidades.lista-necesidades')
             </div>
         </div>
     </div>
+
     @livewire('necesidades.curar-necesidad')
     @livewire('necesidades.ver-necesidad')
+    @livewire('componentes.BuscarEquipo')<!--por que es busqueda por tabla-->
 </x-app-layout>
