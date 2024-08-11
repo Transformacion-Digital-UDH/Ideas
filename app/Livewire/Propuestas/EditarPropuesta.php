c3b2aabeb72c1dcbe6b60fc3b07457954bbcba99
MaDDaIt <2021110395@udh.edu.pe>
2024-08-10 20:49:25 -0500

    Editar Propuesta- listo

--- app/Livewire/Propuestas/EditarPropuesta.php
+++ app/Livewire/Propuestas/EditarPropuesta.php
@@ -15,6 +15,19 @@ class EditarPropuesta extends Component
 
     public $pro_titulo;
     public $pro_descripcion;
+    public $pro_lugar;
+    public $pro_beneficiarios;
+    public $pro_tratar;
+    public $pro_causas;
+    public $pro_consecuencias;
+    public $pro_justificacion;
+    public $pro_aportes;
+    public $problematicas;
+    public $variable_1;
+    public $variable_2;
+    public $pro_tipo;
+    public $pro_estado;
+
 
 
     protected $listeners = ['editar' => 'editar'];
@@ -32,6 +45,18 @@ class EditarPropuesta extends Component
         $this->propuesta = Propuestas::find($id);
         $this->pro_titulo = $this->propuesta->pro_titulo;
         $this->pro_descripcion = $this->propuesta->pro_descripcion;
+        $this->pro_lugar = $this->propuesta->pro_lugar;
+        $this->pro_beneficiarios = $this->propuesta->pro_beneficiarios;
+        $this->pro_tratar = $this->propuesta->pro_tratar;
+        $this->pro_causas = $this->propuesta->pro_causas;
+        $this->pro_consecuencias = $this->propuesta->pro_consecuencias;
+        $this->pro_justificacion = $this->propuesta->pro_justificacion;
+        $this->pro_aportes = $this->propuesta->pro_aportes;
+        $this->problematicas = $this->propuesta->problematicas;
+        $this->variable_1 = $this->propuesta->variable_1;
+        $this->variable_2 = $this->propuesta->variable_2;
+        $this->pro_tipo = $this->propuesta->pro_tipo;
+        $this->pro_estado = $this->propuesta->pro_estado;
         $this->necesidad = Necesidades::find($this->propuesta->nec_id);
     }
 
@@ -43,6 +68,18 @@ class EditarPropuesta extends Component
         ]);
         $this->propuesta->pro_titulo = $this->pro_titulo;
         $this->propuesta->pro_descripcion = $this->pro_descripcion;
+        $this->propuesta->pro_lugar = $this->pro_lugar;
+        $this->propuesta->pro_beneficiarios = $this->pro_beneficiarios;
+        $this->propuesta->pro_tratar = $this->pro_tratar;
+        $this->propuesta->pro_causas = $this->pro_causas;
+        $this->propuesta->pro_consecuencias = $this->pro_consecuencias;
+        $this->propuesta->pro_justificacion = $this->pro_justificacion;
+        $this->propuesta->pro_aportes = $this->pro_aportes;
+        $this->propuesta->problematicas = $this->problematicas;
+        $this->propuesta->variable_1 = $this->variable_1;
+        $this->propuesta->variable_2 = $this->variable_2;
+        $this->propuesta->pro_tipo = $this->pro_tipo;
+        $this->propuesta->pro_estado = $this->pro_estado;
         $this->propuesta->save();
         $this->dispatch('actualizado');
         $this->closeModal();
