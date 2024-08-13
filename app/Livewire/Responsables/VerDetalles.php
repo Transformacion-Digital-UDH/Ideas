0bcb60bc0d2fef401080d5341be024d177286b43
abimaelfv <abimaelfv@hotmail.com>
2024-08-12 22:41:46 -0500

    Desabilitar Modo oscuro y crear mis-responsabilidades

---
+++ app/Livewire/Responsables/VerDetalles.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Responsables;
+
+use Livewire\Component;
+
+class VerDetalles extends Component
+{
+    public function render()
+    {
+        return view('livewire.responsables.ver-detalles');
+    }
+}
