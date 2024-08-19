b4098b2b582d6852b465f103ced6a47e3c104200
abimaelfv <abimaelfv@hotmail.com>
2024-08-19 18:19:43 -0500

    Segunda version

--- app/Livewire/Postulaciones/AsignarPostulante.php
+++ app/Livewire/Postulaciones/AsignarPostulante.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Postulaciones;
 
 use App\Models\Postulaciones;
 use App\Models\Propuestas;
+use App\Models\User;
 use App\Traits\GestionarModal;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
@@ -14,9 +15,14 @@ class AsignarPostulante extends Component
 
     protected $listeners = ['asignar'];
     public $pro_id;
+    public $oficial = false;
     public $propuesta;
     public $existeAsigando;
     public $confirmar = false;
+
+    // Busqueda
+    public $search = '';
+    public $results = [];
 
     public function mount()
     {
@@ -44,6 +50,7 @@ class AsignarPostulante extends Component
 
     public function asignarPostulante($id)
     {
+        exit;
         DB::beginTransaction();
         try {
             $postulacion = Postulaciones::findOrFail($id);
@@ -68,8 +75,24 @@ class AsignarPostulante extends Component
         $this->confirmar = false;
     }
 
+    public function changeOficial()
+    {
+        $this->render();
+    }
+
     public function render()
     {
+        $this->results = $this->buscar();
         return view('livewire.postulaciones.asignar-postulante');
+    }
+
+    public function buscar()
+    {
+        $this->results = User::whereHas('roles', function ($query) {
+            $query->whereIn('name', ['DOCENTE', 'PROYECTISTA', 'VRI', 'ESCUELA']);
+        })
+            ->where('name', 'like', '%' . $this->search . '%')
+            ->limit(10)
+            ->get();
     }
 }
