43a49384e050e56126d525032a045455f78afc72
abimaelfv <abimaelfv@hotmail.com>
2024-10-19 00:41:12 -0500

    Livewire modificado

--- config/livewire.php
+++ config/livewire.php
@@ -20,6 +20,7 @@ return [
     | View Path
     |---------------------------------------------------------------------------
     |
+    |
     | This value is used to specify where Livewire component Blade templates are
     | stored when running file creation commands like `artisan make:livewire`.
     | It is also used if you choose to omit a component's render() method.
@@ -127,7 +128,7 @@ return [
     |
     */
 
-    'inject_assets' => true,
+    'inject_assets' => false,
 
     /*
     |---------------------------------------------------------------------------
