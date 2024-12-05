08c9a6b9ef4794c79f800f14cdfe98233fb0c410
abimaelfv <abimaelfv@hotmail.com>
2024-12-05 17:19:24 -0500

    Titulos de tesis

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ApisController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/titulo/{correo}', [ApisController::class, 'titulo'])->name('titulo');
